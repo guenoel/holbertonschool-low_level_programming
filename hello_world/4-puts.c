0ad5993941efa793f00f02c83d821df1e79efb62
guenoel <guenooel@hotmail.com>
2022-06-07 23:06:30 +0200

    4.2

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,7 +1,12 @@
 #include <stdio.h>
 
-/*fonction principale*/
-int main(void)
+/**
+* main - Fonction principale simple juste pour afficher
+*
+* c'est tellement simple que y a pas grand chose a dire de plus
+* Return: rien du tout !
+*/
+void main(void) /* fonction main */
 {
 	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
