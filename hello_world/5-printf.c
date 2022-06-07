49d6aee2ae3bd2a3afb1270757ed458c9b95688f
guenoel <guenooel@hotmail.com>
2022-06-07 23:15:34 +0200

    5.1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -1,1 +1,14 @@
+#include <stdio.h>
 
+/**
+* main - Fonction principale simple juste pour afficher
+* mais avec printf cette fois
+*
+* c'est tellement simple que y a pas grand chose a dire de plus
+* Return: rien du tout !
+*/
+int main(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art,\n");
+	return (0);
+}
