4eba98ba4f1746396feec5fa13c568ad679c1ed0
guenoel <guenooel@hotmail.com>
2022-06-08 09:50:15 +0200

    5.2

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -14,11 +14,12 @@ int main(void)
 	long int c;
 	long long int d;
 	float e;
-	
-	printf("the size of a char: %lu byte\(s\)\n", (unsigned long)sizeof(a));
-	printf("the size of a int: %lu byte\(s\)\n", (unsigned long)sizeof(b));
-	printf("the size of a long int: %lu byte\(s\)\n", (unsigned long)sizeof(c));
-	printf("the size of a long long int: %lu byte\(s\)\n", (unsigned long)sizeof(d));
-	printf("the size of a float: %lu byte\(s\)\n", (unsigned long)sizeof(e));
+
+	printf("the size of a char: %lu byte(s)\n", (unsigned long)sizeof(a));
+	printf("the size of a int: %lu byte(s)\n", (unsigned long)sizeof(b));
+	printf("the size of a long int: %lu byte(s)\n", (unsigned long)sizeof(c));
+	printf("the size of a long long int: %lu byte(s)\n"
+			, (unsigned long)sizeof(d));
+	printf("the size of a float: %lu byte(s)\n", (unsigned long)sizeof(e));
 	return (0);
 }
