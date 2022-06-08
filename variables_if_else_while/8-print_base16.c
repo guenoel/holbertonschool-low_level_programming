d07f621636c7303eedcee43e8353de584eb805ac
guenoel <guenooel@hotmail.com>
2022-06-08 17:15:01 +0200

    8.1

---
+++ variables_if_else_while/8-print_base16.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	int c;
+	char n;
+
+	c = '\n';
+	for (n = 48; n < 58; n++)
+	putchar(n);
+	for (n = 97; n < 103; n++)
+	putchar(n);
+	putchar(c);
+	return (0);
+}
