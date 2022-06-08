cce9d479daeb1f7f1b1f2970d247fcae56c79587
guenoel <guenooel@hotmail.com>
2022-06-08 00:09:57 -0700

    5.1

--- hello_world/5-printf.c
+++ hello_world/5-printf.c
@@ -9,6 +9,16 @@
 */
 int main(void)
 {
-	printf("with proper grammar, but the outcome is a piece of art,\n");
+	char a;
+	int b;
+	long int c;
+	long long int d;
+	float e;
+	
+	printf("the size of a char: %lu byte\(s\)\n", (unsigned long)sizeof(a));
+	printf("the size of a int: %lu byte\(s\)\n", (unsigned long)sizeof(b));
+	printf("the size of a long int: %lu byte\(s\)\n", (unsigned long)sizeof(c));
+	printf("the size of a long long int: %lu byte\(s\)\n", (unsigned long)sizeof(d));
+	printf("the size of a float: %lu byte\(s\)\n", (unsigned long)sizeof(e));
 	return (0);
 }
