02cc4541f3256a8ede37ee8998e95ad680a9bb74
guenoel <guenooel@hotmail.com>
2022-06-08 15:43:53 +0200

    2.1

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	char c;
+	char n;
+	c = '\n';
+	for (n = 97;n < 123; n++)
+		putchar(n);
+	putchar(c);
+	return (0);
+}
