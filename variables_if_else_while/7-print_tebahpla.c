af6c146dc9490f9d34b60d4c07baaa80d24e8918
guenoel <guenooel@hotmail.com>
2022-06-08 17:05:10 +0200

    7.1

---
+++ variables_if_else_while/7-print_tebahpla.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	int c;
+	int n;
+
+	c = '\n';
+	for (n = 122; n > 96; n--)
+	putchar(n);
+	putchar(c);
+	return (0);
+}
