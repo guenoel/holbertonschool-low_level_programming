0ab02debc2da3985b103d2fd8f374cba2ac6051d
guenoel <guenooel@hotmail.com>
2022-06-09 00:24:36 +0200

    0.1

---
+++ functions_nested_loops/0-putchar.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les fonctions et les boucles
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	printf("_putchar\n");
+	return (0);
+}
---
+++ functions_nested_loops/README.md
@@ -0,0 +1,1 @@
+Exercices sur les fonctions et boucles "nichées" (???)
