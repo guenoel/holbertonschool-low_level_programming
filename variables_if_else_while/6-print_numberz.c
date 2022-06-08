90ed1619de06976369a2e3e11ebb460c00789600
guenoel <guenooel@hotmail.com>
2022-06-08 16:57:28 +0200

    6.1

---
+++ variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	char c;
+	char n;
+
+	c = '\n';
+	for (n = 48; n < 58; n++)
+	putchar(n);
+	putchar(c);
+	return (0);
+}
