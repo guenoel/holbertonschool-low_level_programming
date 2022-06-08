a487ce90db7fd60763f0f0f1ce541786015c44f1
guenoel <guenooel@hotmail.com>
2022-06-08 15:51:10 +0200

    3-print_alphabets.c

---
+++ variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	char c;
+	char n;
+
+	c = '\n';
+	for (n = 97; n < 123; n++)
+		putchar(n);
+	for (n = 65; n < 90; n++)
+		putchar(n);
+	putchar(c);
+	return (0);
+}
