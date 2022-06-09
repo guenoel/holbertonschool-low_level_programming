9c1c9b0674ff5121f2887ffef111b91d1f99166f
guenoel <guenooel@hotmail.com>
2022-06-09 21:10:55 +0200

    c functions 1.1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -1,12 +1,5 @@
-/**
-#ifndef _putchar
-#define _putchar 1
- * titi
- *
- * tata
- */
 int _putchar(char i);
-char *word="_putchar\n";
-/*
-#endif
+/**
+ *char *word="_putchar\n"; (exercice 0)
 */
+int print_alphabet(void);
