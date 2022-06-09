c594833d3c7c2a56a913c95562920cf3d0c33f40
guenoel <guenooel@hotmail.com>
2022-06-10 00:35:02 +0200

    functions in c 4.1

---
+++ functions_nested_loops/4-isalpha.c
@@ -0,0 +1,21 @@
+#include "main.h"
+/**
+ * _islower - recréer la fonction islower
+ * @c: variable du caractere a tester
+ *
+ * exercice sur les fonctions et les boucles
+ * recréer la fonction islower (est-ce une minuscule)
+ * Return: retourne un 0 ou un 1 si min ou maj
+ */
+
+int _isalpha(int c)
+{
+	if (((c >= 97) && (c <= 122)) || ((c >= 65) && (c <= 90)))
+	{
+	return (1);
+	}
+	else
+	{
+	return (0);
+	}
+}
--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -3,5 +3,6 @@ int _putchar(char i);
  * char *word="_putchar\n"; (exercice 0)
  * int print_alphabet(void); (exercice 1)
  * void print_alphabet_x10(void); (exercice 2)
+ * int _islower(int c);
  */
-int _islower(int c);
+int _isalpha(int c);
