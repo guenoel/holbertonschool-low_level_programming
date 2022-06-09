a017a1d447928cf780ca5f870dc266d1bf44817d
guenoel <guenooel@hotmail.com>
2022-06-09 16:13:20 +0200

    functions in c 0.2

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  * exercice sur les fonctions et les boucles
  * Return: retourne un zero
  */
-int i;
-
 int main(void)
 {
-	for (i=0; word[i] != '\0'; i++)
+	int i;
+
+	for (i = 0; word[i] != '\0'; i++)
 	{
 		_putchar(word[i]);
 	}
