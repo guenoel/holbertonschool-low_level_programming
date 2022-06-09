65d7e116935466ad2ee3fa860db1f3360b04650c
guenoel <guenooel@hotmail.com>
2022-06-10 00:27:39 +0200

    functions in c 3.1

--- functions_nested_loops/3-islower.c
+++ functions_nested_loops/3-islower.c
@@ -1,6 +1,7 @@
 #include "main.h"
 /**
  * _islower - recréer la fonction islower
+ * @c: variable du caractere a tester
  *
  * exercice sur les fonctions et les boucles
  * recréer la fonction islower (est-ce une minuscule)
