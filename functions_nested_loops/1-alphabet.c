fec3b3f83bf0f57f0ebdb30e2548fe199b431a1d
guenoel <guenooel@hotmail.com>
2022-06-09 21:11:16 +0200

    c functions 1.1

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - fonction principale
+ *
+ * exercice sur les fonctions et les boucles
+ * Return: retourne un zero
+ */
+int print_alphabet()
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+	return (0);
+}
