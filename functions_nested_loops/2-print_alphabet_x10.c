ca5799d3699908a1a47d7bedaf860909bd67f06a
guenoel <guenooel@hotmail.com>
2022-06-09 22:28:36 +0200

    2.1

---
+++ functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,29 @@
+#include "main.h"
+/**
+ * print_alphabet - fonction affichage d alphabet
+ *
+ * exercice sur les fonctions et les boucles
+ * affiche l'alphabet
+ * Return: retourne un zero
+ */
+int print_alphabet(void)
+{
+	int i;
+
+	for (i = 97; i < 123; i++)
+	{
+		_putchar(i);
+	}
+	_putchar(10);
+	return (0);
+}
+
+void print_alphabet_x10(void)
+{
+	int i;
+
+	for (i =0; i < 11; i++)
+	{
+		print_alphabet();
+	}
+}
