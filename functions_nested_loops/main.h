ae04115c2e7cf145b2602e16383fb00621afb096
guenoel <guenooel@hotmail.com>
2022-06-09 23:48:19 -0700

    functions in c 6.1

--- functions_nested_loops/main.h
+++ functions_nested_loops/main.h
@@ -5,5 +5,7 @@ int _putchar(char i);
  * void print_alphabet_x10(void); (exercice 2)
  * int _islower(int c);
  * int _isalpha(int c);
+ *
+ * int print_sign(int n);
  */
-int print_sign(int n);
+int _abs(int n);
