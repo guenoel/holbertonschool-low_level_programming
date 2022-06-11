700a023c35a5274bd3421cd7cb5353c305abe104
guenoel <guenooel@hotmail.com>
2022-06-11 23:27:32 +0200

    compile in c 101.1

---
+++ hello_world/101-quote.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+
+/**
+* main - Fonction principale simple juste pour afficher
+* mais avec printf cette fois
+*
+* c'est tellement simple que y a pas grand chose a dire de plus
+* Return: rien du tout !
+*/
+int main(void)
+{
+	fprintf(stderr, "and that piece of art is useful\" - Dora Korpar, 2015-10-19");
+	return(1);
+}
