da9d95d4ecf443b61825af383bd41029e0d147cf
guenoel <guenooel@hotmail.com>
2022-06-12 18:58:41 +0200

    advanced loops in c 102.1

---
+++ variables_if_else_while/102-print_comb5.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+/**
+ * main - fonction principale
+ *
+ * exercice sur les conditions
+ * Return: retourne un zero
+ */
+int main(void)
+{
+	int m;
+	int c;
+	int d;
+	int u;
+
+for (m = 0; m <= 9; m++)
+{
+for (c = 0; c <= 9; c++)
+	{
+	for (d = 0; d <= 9; d++)
+	{
+		for (u = 0; u <= 9; u++)
+		{
+			if (m == d && c == u )
+				;
+			else if (m > d || c > u)
+				;
+			else
+			{
+			putchar(m + '0');
+			putchar(c + '0');
+			putchar(' ');
+			putchar(d + '0');
+			putchar(u + '0');
+			if (!(c == 7 && d == 8 && u == 9))
+				{
+				putchar(',');
+				putchar(' ');
+				}
+			}
+		}
+	}
+	}
+}
+	putchar('\n');
+	return (0);
+}
