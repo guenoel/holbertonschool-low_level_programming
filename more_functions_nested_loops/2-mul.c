9ffaf3c1ee0c63a7822866b70579ed721bfbdb3f
guenoel <guenooel@hotmail.com>
2022-06-13 12:24:03 +0200

    ore loops in c 1.1

--- more_functions_nested_loops/2-mul.c
+++ more_functions_nested_loops/2-mul.c
@@ -1,14 +1,15 @@
 #include "main.h"
 /**
- * _isupper - recréer la fonction islower
- * @c: variable du caractere a tester
+ * mul - recréer la fonction islower
+ * @a: variable a multiplier
+ * @b: 2eme variable a multiplier
  *
  * exercice sur les fonctions et les boucles
  * recréer la fonction islower (est-ce une minuscule)
  * Return: retourne un 0 ou un 1 si min ou maj
  */
 
-int mul(int a, int b)
+int mul (int a, int b)
 {
 	int res;
 	res = a * b;
