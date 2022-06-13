5afee919a8ad8d1e22602313c77cc6430ae26711
guenoel <guenooel@hotmail.com>
2022-06-13 14:16:46 +0200

    more 3.1

---
+++ more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,15 @@
+#include "main.h"
+/**
+ * print_numbers - afficher les chiffres
+ *
+ * exercice sur les fonctions et les boucles
+ * recréer la fonction islower (est-ce une minuscule)
+ * Return: retourne un 0 ou un 1 si min ou maj
+ */
+
+void print_numbers(void)
+{
+	for (i = 0; i <= 9; i++)
+		_putchar(i + '0');
+	_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -1,4 +1,6 @@
+char _putchar(int);
 /**
  * int _isupper(int);
+ * int mul(int, int);
  */
-int mul(int, int);
+void print_numbers(void);
