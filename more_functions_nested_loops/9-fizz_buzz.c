1bc05d27f08d8cd601865ffe935d444ac019f2b9
guenoel <guenooel@hotmail.com>
2022-06-13 23:43:18 +0200

    le fichier plus court 9.3

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -9,37 +9,23 @@
 int main(void)
 {
 	int c;
-	int d;
-	int u;
 
-	for (c = 0; c <= 1 ; c++)
-	{
-		for (d = 0; d <= 9; d++)
+	for (c = 1; c <= 100 ; c++)
+	{		
+		if ((c % 3 == 0) && (c % 5 == 0))
+			printf("FizzBuzz ");
+		else if (c % 5 == 0)
 		{
-			for (u = 0; u <= 9; u++)
-			{
-				if ((c >= 1 && (d >= 1 || u >= 1)) || (((c == 0) && (d == 0) && (u == 0))))
-					;
-				else
-				{
-					if ((((c * 100) + (d * 10) + u) % 3 == 0) && (((c * 100) + (d * 10) + u) \
-								% 5 == 0))
-						printf("FizzBuzz ");
-					else if (((c * 100) + (d * 10) + u) % 5 == 0)
-					{
-						if ((c == 1) && (d == 0) && (u == 0))
-						printf("Buzz");
-						else
-						printf("Buzz ");
-					}
-					else if (((c * 100) + (d * 10) + u) % 3 == 0)
-						printf("Fizz ");
-					else
-					{
-						printf("%d ", ((c * 100) + (d * 10) + u));
-					}
-				}
-			}
+			if (c == 100)
+				printf("Buzz");
+			else
+				printf("Buzz ");
+		}
+		else if ((c % 3) == 0)
+			printf("Fizz ");
+		else
+		{
+			printf("%d ", c);
 		}
 	}
 	printf("\n");
