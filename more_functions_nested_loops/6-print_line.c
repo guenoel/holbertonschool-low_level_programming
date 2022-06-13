849056ed36262234cd7a222cc0af54539ac9de09
guenoel <guenooel@hotmail.com>
2022-06-13 17:20:56 +0200

    more functions 6.1

---
+++ more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,24 @@
+#include "main.h"
+/**
+ * print_line - afficher les chiffres
+ * @n: nombres de tirets pour un chiffre positif
+ *
+ * exercice sur les fonctions et les boucles
+ * recréer la fonction islower (est-ce une minuscule)
+ * Return: retourne un 0 ou un 1 si min ou maj
+ */
+
+void print_line(int n)
+{
+	if (n > 0)
+	{
+		while (n > 0)
+		{
+			_putchar('_');
+			n--;
+		}
+		_putchar('\n');
+	}
+	else
+		_putchar('\n');
+}
--- more_functions_nested_loops/main.h
+++ more_functions_nested_loops/main.h
@@ -5,5 +5,6 @@ char _putchar(int);
  * void print_numbers(void);
  * void print_most_numbers(void);
  * void print_most_numbers(void);
- */
-void more_numbers(void);
+ * void more_numbers(void);
+*/
+void print_line(int n);
