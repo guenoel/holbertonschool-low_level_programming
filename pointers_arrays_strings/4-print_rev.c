10bff15adf3935cf3d4bf82a968e87ecee539b64
guenoel <guenooel@hotmail.com>
2022-06-15 00:35:26 +0200

    pointers 4.1

---
+++ pointers_arrays_strings/4-print_rev.c
@@ -0,0 +1,25 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * print_rev - print
+ * @s: pointer de char
+ *
+ * Return: numbers of elements in array
+ */
+
+void print_rev(char *s)
+{
+	int l;
+
+	l = 0;
+	while (s[l] != '\0')
+	{
+		l++;
+	}
+	while (l > 0)
+	{
+		_putchar(s[l]);
+		l--;
+	}
+	_putchar('\n');
+}
