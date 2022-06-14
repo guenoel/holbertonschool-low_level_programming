46c082ce032e8a9988b057936f1ff67c97e87573
guenoel <guenooel@hotmail.com>
2022-06-15 00:22:08 +0200

    pointers 3.1

---
+++ pointers_arrays_strings/3-puts.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * _puts - print
+ * @str: pointer de char
+ *
+ * Return: numbers of elements in array
+ */
+
+void _puts(char *str)
+{
+	int l;
+
+	l = 0;
+	while (str[l] != '\0')
+	{
+		_putchar(str[l]);
+		l++;
+	}
+	_putchar('\n');
+}
