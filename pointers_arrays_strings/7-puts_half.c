b6c6b1c4b222d2b81a1ae755b5d8a6afcfd8286f
guenoel <guenooel@hotmail.com>
2022-06-15 13:13:17 +0200

    array et pointers 7.1

---
+++ pointers_arrays_strings/7-puts_half.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+/**
+ * puts2 - print
+ * @str: chaine de caractere - pointeur vers un array
+ *
+ * Return: rien
+ */
+void puts_half(char *str)
+{
+	int l;
+	int n;
+
+	l = 0;
+	n = 0;
+	while (str[l] != '\0')
+		l++;
+	l = l - 1;
+	while (l >= n)
+	{
+		if (n > (l / 2))
+			printf("%c", str[n]);
+		n++;
+	}
+	printf("\n");
+}
