cc2c5e2e60fb8dfc3379fd610938dac7ba927f20
guenoel <guenooel@hotmail.com>
2022-06-17 00:23:50 +0200

    more pointers 2.1

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -29,10 +29,11 @@ char *_strncpy(char *dest, char *src, int n)
 {
 	int i;
 
-	for (i = 0; src[i] != '\0' && i < n; j++)
+	for (i = 0; src[i] != '\0' && i < n; i++)
 	{
 		dest[i] = src[i];
 	}
-	dest[i + 1] = '\0';
+	if (i < n)
+	dest[i] = '\0';
 	return (dest);
 }
