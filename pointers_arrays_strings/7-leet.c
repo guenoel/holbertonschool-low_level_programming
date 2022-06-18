95f7684548d7e9f1bfdfe5a56820d7cf64d9a196
guenoel <guenooel@hotmail.com>
2022-06-18 13:05:03 +0200

    more functions 9.1

---
+++ pointers_arrays_strings/7-leet.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * *leet - change a string into 1337
+ * @ptr: pointeur vers l'array de char str
+ *
+ * Return: valeur du pointeur ptr (valeur de str)
+ */
+char *leet(char *ptr)
+{
+	int i, j;
+	int a[] = {'A', 'E', 'O', 'T', 'L'};
+	int b[] = {'4', '3', '0', '7', '1'};
+
+	for (i = 0; ptr[i] != '\0'; i++)
+	{
+		for (j = 0; j < 5; j++)
+		{
+			if (ptr[i] == a[j])
+				ptr[i] = b[j];
+		}
+		
+	}
+	return (ptr);
+}
