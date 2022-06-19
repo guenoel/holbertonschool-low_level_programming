51e1e37a3567c3d4bcd2d658a0d1475312185f8c
guenoel <guenooel@hotmail.com>
2022-06-19 18:07:44 +0200

    advanced more pointers

--- pointers_arrays_strings/100-rot13.c
+++ pointers_arrays_strings/100-rot13.c
@@ -9,14 +9,27 @@
  */
 char *rot13(char *ptr)
 {
-	int i;
+	int a, b, i, j, k, l, m;
 
 	for (i = 0; ptr[i] != '\0'; i++)
 	{
-		if ((ptr[i] >= 'a' && ptr[i] <= 'm') || (ptr[i] >= 'A' && ptr[i] <= 'M'))
-			ptr[i] = ptr[i] + 13;
-		else if ((ptr[i] >= 'n' && ptr[i] <= 'z') || (ptr[i] >= 'N' && ptr[i] <= 'Z'))
-			ptr[i] = ptr[i] - 13;
+		j = 'A';
+		k = 'M';
+		l = 'a';
+		m = 'm';
+		b = 1;
+		a = 0;
+		while (a < 2)
+			if ((ptr[i] >= j && ptr[i] <= k) || (ptr[i] >= l && ptr[i] <= m))
+			{
+				ptr[i] = ptr[i] + (b * 13);
+			}
+		j = j + 13;
+		k = k + 13;
+		l = l + 13;
+		m = m + 13;
+		b = (-1);
+		a++;
 	}
 	return (ptr);
 }
