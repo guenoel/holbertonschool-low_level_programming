33eddfdcefd288c0ece4f5d86423574e198b8ab4
guenoel <guenooel@hotmail.com>
2022-06-21 00:39:27 +0200

    more & more pointers 5.1

--- pointers_arrays_strings/5-strstr.c
+++ pointers_arrays_strings/5-strstr.c
@@ -15,6 +15,7 @@ char *_strstr(char *haystack, char *needle)
 
 	for (c = 0; needle[c] != '\0'; c++)
 		;
+		printf("c = %d\n", c);
 	for (i = 0, b = 0, j = 0; haystack[i] != '\0'; i++)
 	{
 		if (haystack[i] == needle[j])
@@ -22,9 +23,14 @@ char *_strstr(char *haystack, char *needle)
 			j++;
 			b++;
 		}
+		printf("c = %d\n", c);
 		if (b == c)
 		{
-			return (&haystack[i - c + 1]);
+			if (c > 0)
+			{
+				c = c - 1;
+			}
+			return (&haystack[i - c]);
 		}
 	}
 	return (NULL);
