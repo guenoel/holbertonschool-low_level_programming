de07b011880d6415726a066ddcbca1b56cbe7fc2
guenoel <guenooel@hotmail.com>
2022-06-20 11:20:21 +0200

    more & more pointers 0.1

--- pointers_arrays_strings/0-memset.c
+++ pointers_arrays_strings/0-memset.c
@@ -3,16 +3,17 @@
 
 /**
  * *_memset - prints buffer in hexa
- * @s: pointer to memory area  
+ * @s: pointer to memory area
  * @b: data to write to memory
  * @n: numbers of bytes
  *
  * Return: something
  */
-unsigned int i;
 
 char *_memset(char *s, char b, unsigned int n)
 {
+unsigned int i;
+
 for (i = 0; i < n; i++)
 	s[i] = b;
 return (s);
