32b44fbcdae815edf78db073a6f0520319c69c65
guenoel <guenooel@hotmail.com>
2022-06-21 00:15:58 +0200

    more & more pointers 5.1

---
+++ pointers_arrays_strings/5-strstr.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strstr - finds the first occurrence of the substring
+ * needle in the string haystack
+ * @haystack: substring to find
+ * @needle: string where we search occurence
+ *
+ * Return: something
+ */
+char *_strstr(char *haystack, char *needle)
+{
+	unsigned int i, j, b, c;
+
+	for (c = 0; needle[c] != '\0'; c++)
+		;
+	for (i = 0, b = 0, j = 0; haystack[i] != '\0'; i++)
+	{
+		if (haystack[i] == needle[j])
+		{
+			j++;
+			b++;
+		}
+		if (b == c)
+		{
+			return (&haystack[i - c + 1]);
+		}
+	}
+	return (NULL);
+}
