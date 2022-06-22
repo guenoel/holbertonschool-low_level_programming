11e258ee14dabdc5c407d10fe876f87f6ba48ee8
guenoel <guenooel@hotmail.com>
2022-06-22 15:33:07 +0200

    recursion 2.1

---
+++ recursion/2-strlen_recursion.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * _strlen_recursion - recursion
+ * @s: string
+ *
+ * Return: nothing
+ */
+int _strlen_recursion(char *s)
+{
+	if (s[0] == '\0')
+		return (0);
+	return (1 + _strlen_recursion(s + 1));
+}
