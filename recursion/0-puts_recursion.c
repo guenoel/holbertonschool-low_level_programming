0e35a725dcc1b3960703924ec67f32dc5f7d0339
guenoel <guenooel@hotmail.com>
2022-06-22 12:04:36 +0200

    Recursion 0.1

---
+++ recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * *_puts_recursion - recursion
+ * @s: string
+ *
+ * Return: nothing
+ */
+
+void _puts_recursion(char *s)
+{
+	int i;
+
+	for (i = 0; s[i] != '\0'; i++)
+		_putchar(s[i]);
+	_putchar('\n');
+}
---
+++ recursion/README.md
@@ -0,0 +1,1 @@
+Exercices sur la recursion
