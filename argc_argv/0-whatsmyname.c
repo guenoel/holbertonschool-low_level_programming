fa77e49b460f374b97186aebd5295f99e4cb808e
guenoel <guenooel@hotmail.com>
2022-06-24 17:32:01 +0200

    argc argv

---
+++ argc_argv/0-whatsmyname.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main- fonction principale
+ * @argc: nb arguments
+ * @argv: array d arguments
+ *
+ * return : 0 si tout va bien
+ */
+
+
+int main(int argc, char *argv[])
+{
+	if (argc > 1)
+	{
+		printf("%s\n", argv[1]);
+	}
+	else
+		printf("%s\n", argv[0]);
+	return (0);
+}
---
+++ argc_argv/README.md
@@ -0,0 +1,1 @@
+Exercices sur argc argv
