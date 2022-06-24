78f9b54dd4354b4d91fd8fbb3ee6063adf2fa127
guenoel <guenooel@hotmail.com>
2022-06-24 22:15:04 +0200

    3.1

---
+++ argc_argv/3-mul.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main- fonction principale
+ * @argc: nb arguments
+ * @argv: array d arguments
+ *
+ * Return: 0 si tout va bien
+ */
+
+
+int main(int argc, char *argv[])
+{
+	int i, j;
+
+	if (argc == 3)
+	{
+		i = atoi(argv[1]);
+		j = atoi(argv[2]);
+		printf("%d\n", (i * j));
+	}
+	else
+	{
+		printf("Error\n");
+		return (1);
+	}
+	return (0);
+}
