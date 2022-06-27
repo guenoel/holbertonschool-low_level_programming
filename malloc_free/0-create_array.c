620432bf8bf5d62b017b819e63804d7d5b64f28c
guenoel <guenooel@hotmail.com>
2022-06-27 12:07:48 +0200

    malloc 0.1

--- malloc_free/0-create_array.c
+++ malloc_free/0-create_array.c
@@ -12,7 +12,9 @@ char *create_array(unsigned int size, char c)
 {
 	unsigned int i;
 	char *a;
-
+	
+	if (size == 0)
+		return (NULL);
 	a = malloc(size * sizeof(char));
 	for (i = 0; i < size; i++)
 		a[i] = c;
