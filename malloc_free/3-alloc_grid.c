feff191c364d42633750e5e91df7e7fe250fa06a
guenoel <guenooel@hotmail.com>
2022-06-28 20:09:19 +0200

    malloc 3.1

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -21,7 +21,9 @@ int **alloc_grid(int width, int height)
 		return (NULL);
 	for (i = 0; i < height; i++)
 	{
-		p[i] = malloc(sizeof(int));
+		p[i] = malloc(sizeof(int) * width);
+		if (p == NULL)
+			return (NULL);
 		for (j = 0; j < width; j++)
 		{
 			p[i][j] = 0;
