7755daa7048c551f228b905327fd2c986103eaff
guenoel <guenooel@hotmail.com>
2022-07-01 22:15:31 +0200

    struct 4.1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -1,6 +1,7 @@
 #include "dog.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * new_dog - fonction
  *
@@ -11,8 +12,15 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
+	dog_t *dog;
+	dog_t *dog1;
+	dog = malloc(sizeof(dog_t));
 	dog-> name = name;
 	dog-> age = age;
 	dog-> owner = owner;
 
+	dog1 = &(*dog);
+	dog = malloc(sizeof(dog_t));
+	dog = dog1;
+	return (dog);
 }
