4d76f7a847d08bb28a827af5d9931b80a75e53e3
guenoel <guenooel@hotmail.com>
2022-07-01 16:19:53 +0200

    struct 2.1

---
+++ structures_typedef/2-print_dog.c
@@ -0,0 +1,19 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * print_dog - fonction
+ * struct dog - struct
+ *
+ * @d: pointeur
+ *
+ */
+void print_dog(struct dog *d)
+{
+	if (d == NULL)
+		return;
+
+	printf("Name: %s\n", d->name);
+	printf("Age: %f\n", d->age);
+	printf("Owner: %s\n", d->owner);
+}
