e76554c47a6c64b0c1a325db9048d0ba918bf608
guenoel <guenooel@hotmail.com>
2022-07-01 10:48:07 -0700

    struct 4

---
+++ structures_typedef/4-new_dog.c
@@ -0,0 +1,18 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * new_dog - fonction
+ *
+ * @name: pointeur
+ * @age: age
+ * @owner: owner
+ *
+ */
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog-> name = name;
+	dog-> age = age;
+	dog-> owner = owner;
+
+}
