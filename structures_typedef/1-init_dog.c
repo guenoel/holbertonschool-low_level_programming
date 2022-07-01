a33314bd16a765bce651da717cf3b2347b883347
guenoel <guenooel@hotmail.com>
2022-07-01 15:15:10 +0200

    struct 1.1

--- structures_typedef/1-init_dog.c
+++ structures_typedef/1-init_dog.c
@@ -8,12 +8,16 @@
  * @name: name
  * @age: age
  * @owner: owner
- * 
+ * @d: pointeur
+ *
  */
 
 void init_dog(struct dog *d, char *name, float age, char *owner)
 {
+	if (name != NULL)
 	d->name = name;
+	if (age != NULL)
 	d->age = age;
+	if (owner != NULL)
 	d->owner = owner;
 }
