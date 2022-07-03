3e8d8e8a18b6f3b9fc278fd416e0567c711c6117
guenoel <guenooel@hotmail.com>
2022-07-03 10:13:42 +0200

    struct 4.1

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -13,31 +13,31 @@
  */
 dog_t *new_dog(char *name, float age, char *owner)
 {
-    dog_t *dog;
-    int i, j;
+	dog_t *dog;
+	int i, j;
 
-    dog = malloc(sizeof(dog_t));
-    if (dog == NULL)
-        return (NULL);
+	dog = malloc(sizeof(dog_t));
+	if (dog == NULL)
+		return (NULL);
 
-    i = strlen(name) + 1;
-    j = strlen(owner) + 1;
-    dog->name = malloc(sizeof(char) * i);
-    if (dog->name == NULL)
-    {
-        free(dog);
-        return (NULL);
-    }
-    strcpy(dog->name, name);
-    dog->age = age;
+	i = strlen(name) + 1;
+	j = strlen(owner) + 1;
+	dog->name = malloc(sizeof(char) * i);
+	if (dog->name == NULL)
+	{
+		free(dog);
+		return (NULL);
+	}
+	strcpy(dog->name, name);
+	dog->age = age;
 
-    dog->owner = malloc(sizeof(char) * j);
-    if (dog->owner == NULL)
-    {
-        free(dog->name);
-        free(dog);
-        return (NULL);
-    }
-    strcpy(dog->owner, owner);
-    return dog;
+	dog->owner = malloc(sizeof(char) * j);
+	if (dog->owner == NULL)
+	{
+		free(dog->name);
+		free(dog);
+		return (NULL);
+	}
+	strcpy(dog->owner, owner);
+	return dog;
 }
