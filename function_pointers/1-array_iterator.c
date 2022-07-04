ab6ac1a727bb26d3096e42a039edcfddf6ad103f
guenoel <guenooel@hotmail.com>
2022-07-04 13:44:52 +0200

    functions pointers 1.1

---
+++ function_pointers/1-array_iterator.c
@@ -0,0 +1,24 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * array_iterator - fonction
+ *
+ * @array: array
+ * @size: pointer
+ * @action: pointer to function
+ *
+ * Return: rien du tout
+ */
+
+void array_iterator(int *array, size_t size, void (*action)(int))
+{
+	long unsigned int i;
+
+	if (action == NULL)
+		return;
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	action(array[i]);
+}
