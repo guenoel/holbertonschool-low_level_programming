3db2a0032777edd379e4493cadb10c542dbbd6de
guenoel <guenooel@hotmail.com>
2022-07-05 19:23:43 +0200

    function pointer 3.1

--- function_pointers/3-main.c
+++ function_pointers/3-main.c
@@ -25,7 +25,11 @@ int main(int argc, char **argv)
 	a = atoi(argv[1]);
 	b = atoi(argv[3]);
 
-	if (!((strcmp(argv[2], "+") == 0) || (strcmp(argv[2], "-") == 0) || (strcmp(argv[2], "*") == 0) || (strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)))
+	if (!((strcmp(argv[2], "+") == 0) || 
+				(strcmp(argv[2], "-") == 0) || 
+				(strcmp(argv[2], "*") == 0) || 
+				(strcmp(argv[2], "/") == 0) || 
+				(strcmp(argv[2], "%") == 0)))
 	{
 		printf("Error\n");
 		exit(99);
