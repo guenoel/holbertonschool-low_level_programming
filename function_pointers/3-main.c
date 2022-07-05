45292f4a3118be5fa280526dbfccfda598229f36
guenoel <guenooel@hotmail.com>
2022-07-05 16:27:43 +0200

    function pointer 3.1

---
+++ function_pointers/3-main.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stddef.h>
+#include <string.h>
+#include "3-calc.h"
+/**
+ * main - Fonction principale
+ *
+ * @argc: number of arguments
+ * @argv: argument vector
+ *
+ * Return: zero si tout va bien
+ */
+
+int main(int argc, char **argv)
+{
+	int a, b, c;
+
+	if (argc != 4)
+	{
+		printf("Error\n");
+		exit(98);
+	}
+
+	a = atoi(argv[1]);
+	b = atoi(argv[3]);
+
+	if (b == 0 && ((strcmp(argv[2], "/") == 0) || (strcmp(argv[2], "%") == 0)))
+	{
+		printf("Error\n");
+		exit(100);
+	}
+
+	c = get_op_func(argv[2])(a, b);
+	printf("%d\n", c);
+	return (0);
+}
---
+++ function_pointers/3-op_functions.c
@@ -0,0 +1,71 @@
+/**
+ * op_add - add
+ * @a: variable
+ * @b: variable
+ *
+ * Return: c (result)
+ */
+
+int op_add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+	return (c);
+}
+/**
+ * op_sub - sub
+ * @a: variable
+ * @b: variable
+ *
+ * Return: c (result)
+ */
+int op_sub(int a, int b)
+{
+	int c;
+
+	c = a - b;
+	return (c);
+}
+/**
+ * op_mul - mul
+ * @a: variable
+ * @b: variable
+ *
+ * Return: c (result)
+ */
+int op_mul(int a, int b)
+{
+	int c;
+
+	c = a * b;
+	return (c);
+}
+/**
+ * op_div - div
+ * @a: variable
+ * @b: variable
+ *
+ * Return: c (result)
+ */
+int op_div(int a, int b)
+{
+	int c;
+
+	c = a / b;
+	return (c);
+}
+/**
+ * op_mod - mod
+ * @a: variable
+ * @b: variable
+ *
+ * Return: c (result)
+ */
+int op_mod(int a, int b)
+{
+	int c;
+
+	c = a % b;
+	return (c);
+}
