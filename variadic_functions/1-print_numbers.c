72aaba115c5714def9a2a8a216daabd8528e455d
guenoel <guenooel@hotmail.com>
2022-07-05 20:39:05 +0200

    variadic 1.1

---
+++ variadic_functions/1-print_numbers.c
@@ -0,0 +1,30 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdarg.h>
+
+/**
+ * print_numbers - Function
+ *
+ * @separator: au moins une variable obligatoire
+ * @n: variable
+ *
+ * Return: rien
+ */
+
+void print_numbers(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+
+	va_list argp;
+
+	va_start(argp, n);
+
+	for (i = 0; i < n; i++)
+	{
+		printf("%d", va_arg(argp, int));
+	if (i < (n - 1))
+		printf("%s", separator);
+	}
+	va_end(argp);
+	printf("\n");
+}
