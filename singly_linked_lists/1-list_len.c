4cd69b70e0afc0e6a80a4188460c39e0f2d9eab4
guenoel <guenooel@hotmail.com>
2022-07-15 11:05:05 +0200

    linked list - 1

---
+++ singly_linked_lists/1-list_len.c
@@ -0,0 +1,17 @@
+#include "lists.h"
+
+/**
+ * print_list - function
+ * @h: list
+ *
+ * Return: Number of bytes printed
+ */
+
+size_t list_len(const list_t *h)
+{
+	int i;
+
+	for (i = 0; h != NULL; i++)
+		h = h->next;
+	return (i);
+}
