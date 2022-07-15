4dff4367f35b9bd1141c56eef08789693510ad04
guenoel <guenooel@hotmail.com>
2022-07-15 18:38:11 +0200

    linked list 4

--- singly_linked_lists/4-free_list.c
+++ singly_linked_lists/4-free_list.c
@@ -2,10 +2,13 @@
 
 /**
  * free_list - function
+ * @head: la liste a liberer
  */
 
 void free_list(list_t *head)
 {
+	if (head == NULL)
+		return;
 	if (head->next != NULL)
 		free_list(head->next);
 	free(head->str);
