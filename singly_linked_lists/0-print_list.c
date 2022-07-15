00e3075edb69b060bbc75fc061b6485e6f5e7551
guenoel <guenooel@hotmail.com>
2022-07-15 10:50:19 +0200

    linked list - 0

--- singly_linked_lists/0-print_list.c
+++ singly_linked_lists/0-print_list.c
@@ -14,13 +14,9 @@ size_t print_list(const list_t *h)
 	for (i = 0; h != NULL; i++)
 	{
 		if (h->str != NULL)
-		{
 			printf("[%d] %s\n", h->len, h->str);
-		}
 		else
-		{
 			printf("[0] (nil)\n");
-		}
 		h = h->next;
 	}
 	return (i);
