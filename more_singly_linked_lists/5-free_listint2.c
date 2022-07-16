cba4aac7252ef6034a306aa28d1dcea5e567b173
guenoel <guenooel@hotmail.com>
2022-07-16 15:47:13 +0200

    more linked list 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -7,9 +7,18 @@
 
 void free_listint2(listint_t **head)
 {
-	if (head == NULL)
+	listint_t *tmp;
+
+	if ((*head) != NULL)
+	{
+		while((*head) != NULL)
+		{
+			tmp = *head;
+			*head = (*head)->next;
+			free(tmp);
+		}
+	}
+	else
 		return;
-	if (head->next != NULL)
-		free_listint(head->next);
-	free(head);
+	*head = NULL;
 }
