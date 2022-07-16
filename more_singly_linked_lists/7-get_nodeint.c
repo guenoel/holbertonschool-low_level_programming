700d50048efccac6e272dba2557417bb44c01959
guenoel <guenooel@hotmail.com>
2022-07-16 17:43:16 +0200

    more single lists 7

--- more_singly_linked_lists/7-get_nodeint.c
+++ more_singly_linked_lists/7-get_nodeint.c
@@ -16,6 +16,10 @@ listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
 		return (NULL);
 
 	for (i = 0; i < index; i++)
-	head = head->next;
+	{
+		if (head->next == NULL)
+			return (NULL);
+		head = head->next;
+	}
 	return (head);
 }
