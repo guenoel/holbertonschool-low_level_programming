c29a389498503cb482ed07c5e694ac05b69928c1
guenoel <guenooel@hotmail.com>
2022-07-16 10:39:02 +0200

    more linked list 0

--- more_singly_linked_lists/0-print_listint.c
+++ more_singly_linked_lists/0-print_listint.c
@@ -9,12 +9,12 @@
 
 size_t print_listint(const listint_t *h)
 {
-    int i;
+	int i;
 
-    for (i = 0; h != NULL; i++)
-    {   
-        printf("%d\n", h->n);
-        h = h->next;
-    }   
-    return (i);
+	for (i = 0; h != NULL; i++)
+	{
+		printf("%d\n", h->n);
+		h = h->next;
+	}
+	return (i);
 }
