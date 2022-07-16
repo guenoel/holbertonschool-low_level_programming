a5d1e2f404ac1f6bd8de2271f26001b6345315dd
guenoel <guenooel@hotmail.com>
2022-07-16 16:05:57 +0200

    more linked list 5

--- more_singly_linked_lists/5-free_listint2.c
+++ more_singly_linked_lists/5-free_listint2.c
@@ -9,19 +9,13 @@ void free_listint2(listint_t **head)
 {
 	listint_t *tmp;
 
-	if ((*head) != NULL)
-	{
-		while ((*head) != NULL)
-		{
-			tmp = *head;
-			*head = (*head)->next;
-			free(tmp);
-		}
-	}
-	else
-	{
-	printf("Freed !\n");
+	if (*head == NULL)
 		return;
+	while ((*head) != NULL)
+	{
+		tmp = *head;
+		*head = (*head)->next;
+		free(tmp);
 	}
 	*head = NULL;
 }
