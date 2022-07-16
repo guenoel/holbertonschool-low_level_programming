f133b22c1b840199307116b7074109fe6e8e9b0c
guenoel <guenooel@hotmail.com>
2022-07-16 17:06:55 +0200

    more single lists 6

--- more_singly_linked_lists/6-pop_listint.c
+++ more_singly_linked_lists/6-pop_listint.c
@@ -12,8 +12,8 @@ int pop_listint(listint_t **head)
 
 	if (head == NULL)
 		return (-1);
-/*	if ((*head)->n == NULL)
-		return (0);*/
+	if ((*head) == NULL)
+		return (0);
 
 	i = (*head)->n;
 	*head = (*head)->next;
