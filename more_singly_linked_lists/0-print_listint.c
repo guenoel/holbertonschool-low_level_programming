f79cb3cf71fcf90a2871401fc0508d5d7e1bb951
guenoel <guenooel@hotmail.com>
2022-07-16 10:35:29 +0200

    more linked list 0

---
+++ more_singly_linked_lists/0-print_listint.c
@@ -0,0 +1,20 @@
+#include "lists.h"
+
+/**
+ * print_listint - function
+ * @h: list
+ *
+ * Return: Number of bytes printed
+ */
+
+size_t print_listint(const listint_t *h)
+{
+    int i;
+
+    for (i = 0; h != NULL; i++)
+    {   
+        printf("%d\n", h->n);
+        h = h->next;
+    }   
+    return (1);
+}
