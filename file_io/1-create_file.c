567374d6bdeed4b415bf0470fec72a7345967b28
guenoel <guenooel@hotmail.com>
2022-07-21 13:15:06 +0200

    file io 1

---
+++ file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - function
+ * @filename: fichier texte
+ * @text_content: nombre de lettre a afficher
+ *
+ * Return: nombre de lettre rellement affichées
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int i, fd;
+
+	if (filename == NULL)
+		return (0);
+	for (i = 0; text_content[i] != '\0'; i++)
+		;
+
+	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+	if (fd == -1)
+		return (0);
+
+	write(fd, text_content, i);
+
+	close(fd);
+	return (1);
+}
