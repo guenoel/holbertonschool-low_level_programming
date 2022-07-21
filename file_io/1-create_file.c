e9951b631c0fc27b69fc52bb7a10b6f8b601a3d4
guenoel <guenooel@hotmail.com>
2022-07-21 13:19:14 +0200

    file io 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -19,7 +19,7 @@ int create_file(const char *filename, char *text_content)
 
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
-		return (0);
+		return (-1);
 
 	write(fd, text_content, i);
 
