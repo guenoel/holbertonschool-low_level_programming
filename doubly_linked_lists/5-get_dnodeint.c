46b61117e57f66b4625e22f90b223b4685892d2e
guenoel <guenooel@hotmail.com>
2022-07-22 00:32:50 +0200

    double linked lists 5

--- doubly_linked_lists/5-get_dnodeint.c
+++ doubly_linked_lists/5-get_dnodeint.c
@@ -1,14 +1,14 @@
 #include "lists.h"
 
 /**
- * get_nodeint_at_index - function
+ * get_dnodeint_at_index - function
  * @head: liste
  * @index: no de node commencant par 0
  *
  * Return: pointer to the node of index
  */
 
-listint_t *get_nodeint_at_index(listint_t *head, unsigned int index)
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int i;
 
