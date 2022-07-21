8660b35f10c3da0639f2e419467ed69ab8373cf4
guenoel <guenooel@hotmail.com>
2022-07-22 00:04:00 +0200

    double linked lists 3

--- doubly_linked_lists/3-add_dnodeint_end.c
+++ doubly_linked_lists/3-add_dnodeint_end.c
@@ -1,18 +1,17 @@
 #include "lists.h"
 
 /**
- * add_nodeint_end - function
+ * add_dnodeint_end - function
  * @head: list
  * @n: integer
  *
  * Return: pointer to new node
  */
 
-listint_t *add_nodeint_end(listint_t **head, const int n)
+dlistint_t *add_dnodeint_end(dlistint_t **head, const int n)
 {
-	listint_t *tmp = *head;
-
-	listint_t *node = malloc(sizeof(listint_t));
+	dlistint_t *tmp = *head;
+	dlistint_t *node = malloc(sizeof(dlistint_t));
 
 	if (!(node))
 		return (NULL);
@@ -21,10 +20,13 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	if ((*head) == NULL)
 	{
 		(*head) = node;
+		node->prev = NULL;
+		node->next = NULL;
 		return (node);
 	}
 	while (tmp->next != NULL)
 		tmp = tmp->next;
+	node->prev = tmp;
 	tmp->next = node;
 	return (node);
 }
