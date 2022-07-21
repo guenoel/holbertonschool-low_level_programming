398d87a4b79a59fcfe4c94929946d9e3229bdb0b
guenoel <guenooel@hotmail.com>
2022-07-21 13:28:47 +0200

    file io 1

--- file_io/1-create_file.c
+++ file_io/1-create_file.c
@@ -16,7 +16,8 @@ int create_file(const char *filename, char *text_content)
 		return (0);
 	for (i = 0; text_content[i] != '\0'; i++)
 		;
-
+	if (text_content == NULL)
+		i = 0;
 	fd = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
 	if (fd == -1)
 		return (-1);
