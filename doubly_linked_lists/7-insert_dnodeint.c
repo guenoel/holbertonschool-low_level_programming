867c6099be11ea0a52e552f045121461a3395017
guenoel <guenooel@hotmail.com>
2022-07-22 17:39:49 +0200

    double linked list 7

--- doubly_linked_lists/7-insert_dnodeint.c
+++ doubly_linked_lists/7-insert_dnodeint.c
@@ -11,13 +11,20 @@
 
 dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 {
-	unsigned int i;
+	unsigned int i, j;
 	dlistint_t *node;
+	dlistint_t *cnt;
 	dlistint_t *tmp;
 	dlistint_t *tmp2;
 	dlistint_t *start;
 
 	if (head == NULL)
+		return (NULL);
+
+	cnt = *head;
+	for (i = 0; (*cnt)->next != NULL, i++)
+		*cnt = (*cnt)->next;
+	if (i > idx)
 		return (NULL);
 
 	node = malloc(sizeof(dlistint_t));
@@ -31,27 +38,27 @@ dlistint_t *insert_dnodeint_at_index(dlistint_t **head, unsigned int idx, int n)
 	{
 		if ((*head) != NULL)
 		{
-		start = *head;
-		for (i = 0; i < (idx - 1); i++)
-		{
-			if ((*head)->next == NULL)
-				return (NULL);
+			start = *head;
+			for (j = 0; j < (idx - 1); j++)
+			{
+				if ((*head)->next == NULL)
+					return (NULL);
+				*head = (*head)->next;
+			}
+			tmp2 = *head; /*head = celui d avant*/
+			tmp = (*head)->next;
+			(*head)->next = node;
 			*head = (*head)->next;
-		}
-		tmp2 = *head; /*head = celui d avant*/
-		tmp = (*head)->next;
-		(*head)->next = node;
-		*head = (*head)->next;
 
-		(*head)->prev = tmp2; /*head = celui ajouté*/
-		(*head)->next = tmp;
-		tmp = (*head);
-		if (tmp != NULL) /*si head est pas placé a la fin*/
-		{
-			*head = (*head)->next;
-			(*head)->prev = tmp; /*head = celui d'après*/
-		}
-		*head = start;
+			(*head)->prev = tmp2; /*head = celui ajouté*/
+			(*head)->next = tmp;
+			tmp = (*head);
+			if (tmp != NULL) /*si head est pas placé a la fin*/
+			{
+				*head = (*head)->next;
+				(*head)->prev = tmp; /*head = celui d'après*/
+			}
+			*head = start;
 		}
 	}
 	else
