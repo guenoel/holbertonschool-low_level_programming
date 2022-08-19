707f0f775af255ffd9310d3124681b4ee2eb6b07
guenoel <guenooel@hotmail.com>
2022-08-19 14:15:44 +0200

    task 0 hash lists

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -2,11 +2,8 @@
 
 hash_table_t *hash_table_create(unsigned long int size)
 {
-	hash_node_t *node;
-
-	hash_table_t *main_table = malloc(sizeof(main_table) * size);
+	hash_table_t *main_table = malloc(sizeof(hash_table_t));
 	main_table->size = size;
-	main_table->array = &node;
-
+	main_table->array = calloc(main_table->size, sizeof(hash_node_t*));
 	return (main_table);
 }
