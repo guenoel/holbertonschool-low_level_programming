2794d4230b06918e9a583b3b05321e451050b83e
guenoel <guenooel@hotmail.com>
2022-08-19 14:20:39 +0200

    task 0 hash lists

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -1,9 +1,19 @@
 #include "hash_tables.h"
 
+/**
+ * hash_table_create - functon to create hash table
+ * @size: size of the array
+ *
+ * Return: pointer to the table
+ */
+
 hash_table_t *hash_table_create(unsigned long int size)
 {
 	hash_table_t *main_table = malloc(sizeof(hash_table_t));
+
+	if (!(main_table))
+		return (NULL);
 	main_table->size = size;
-	main_table->array = calloc(main_table->size, sizeof(hash_node_t*));
+	main_table->array = calloc(main_table->size, sizeof(hash_node_t *));
 	return (main_table);
 }
