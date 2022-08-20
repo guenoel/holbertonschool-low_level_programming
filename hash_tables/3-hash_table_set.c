6f99d9e7877adb003fa6eebc3be338cdffad82f4
guenoel <guenooel@hotmail.com>
2022-08-20 22:09:33 +0200

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -19,8 +19,8 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
 		return (0);
-	new_node->key = (char *)key;
-	new_node->value = (char *)value;
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
 	new_node->next = NULL;
 
 	if (ht->array[index] == NULL)
