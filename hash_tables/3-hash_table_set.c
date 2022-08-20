eac6aa1f0f6fdc7a82b0a1915b08c5870000c29c
guenoel <guenooel@hotmail.com>
2022-08-20 21:49:29 +0200

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -27,7 +27,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = new_node;
 	else
 	{
-		while (ht->array[index] != NULL)
+		while ((ht->array[index])->next != NULL)
 			ht->array[index] = (ht->array[index])->next;
 		(ht->array[index])->next = new_node;
 	}
