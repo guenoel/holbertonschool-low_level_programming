59984424e291b258886949698efa93aa0f3b45ff
guenoel <guenooel@hotmail.com>
2022-08-21 15:30:52 +0200

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -43,9 +43,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 		ht->array[index] = create_node(key, value);
 	else
 	{
-		while(node->next != NULL)
+		while(node != NULL)
 		{
-			if (node->key != key)
+			if (strcmp(node->key, key))
 				node = node->next;
 			else
 			{
