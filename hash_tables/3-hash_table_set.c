587e8f37857134ef8e92b71798bfd4a4dcbe1d8e
guenoel <guenooel@hotmail.com>
2022-08-21 23:12:30 +0200

    task 6

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -55,6 +55,7 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 				node = node->next;
 			else
 			{
+				free(node->value);
 				node->value = strdup(value);
 				return (1);
 			}
