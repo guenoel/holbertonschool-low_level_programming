3cd871908dc00a29dc5af46b5e361567af7313a7
guenoel <guenooel@hotmail.com>
2022-08-21 15:36:51 +0200

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -13,7 +13,7 @@ hash_node_t *create_node(const char *key, const char *value)
 
 	new_node = malloc(sizeof(hash_node_t));
 	if (new_node == NULL)
-		return (0);
+		return (NULL);
 	new_node->key = strdup(key);
 	new_node->value = strdup(value);
 	new_node->next = NULL;
@@ -36,14 +36,20 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 	hash_node_t *new_node;
 	int index;
 
+	if (ht == NULL)
+		return (0);
 	index = key_index((unsigned char *)key, ht->size);
 	node = ht->array[index];
 
 	if (node == NULL)
+	{
 		ht->array[index] = create_node(key, value);
+		if (ht->array[index] == NULL)
+			return (0);
+	}
 	else
 	{
-		while(node != NULL)
+		while (node != NULL)
 		{
 			if (strcmp(node->key, key))
 				node = node->next;
@@ -54,7 +60,9 @@ int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 			}
 		}
 		new_node = create_node(key, value);
-		new_node->next = ht->array[index];	
+		if (ht->array[index] == NULL)
+			return (0);
+		new_node->next = ht->array[index];
 		ht->array[index] = new_node;
 	}
 	return (1);
