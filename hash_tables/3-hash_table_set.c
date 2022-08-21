482b7d03c2174291fd220985a2b6f7a29815bbdb
guenoel <guenooel@hotmail.com>
2022-08-21 14:35:03 +0200

    task 3

--- hash_tables/3-hash_table_set.c
+++ hash_tables/3-hash_table_set.c
@@ -1,4 +1,25 @@
 #include "hash_tables.h"
+/**
+ * create_node - create a new node initialised
+ * @key: key
+ * @value: value
+ *
+ * Return: pointer to new node
+ */
+
+hash_node_t *create_node(const char *key, const char *value)
+{
+	hash_node_t *new_node;
+
+	new_node = malloc(sizeof(hash_node_t));
+	if (new_node == NULL)
+		return (0);
+	new_node->key = strdup(key);
+	new_node->value = strdup(value);
+	new_node->next = NULL;
+
+	return (new_node);
+}
 
 /**
  * hash_table_set - function that add an element in hash table
@@ -11,25 +32,30 @@
 
 int hash_table_set(hash_table_t *ht, const char *key, const char *value)
 {
+	hash_node_t *node;
 	hash_node_t *new_node;
 	int index;
 
 	index = key_index((unsigned char *)key, ht->size);
+	node = ht->array[index];
 
-	new_node = malloc(sizeof(hash_node_t));
-	if (new_node == NULL)
-		return (0);
-	new_node->key = strdup(key);
-	new_node->value = strdup(value);
-	new_node->next = NULL;
-
-	if (ht->array[index] == NULL)
-		ht->array[index] = new_node;
+	if (node == NULL)
+		ht->array[index] = create_node(key, value);
 	else
 	{
-		new_node->next = (ht->array[index]);
-		(ht->array[index]) = new_node;
+		while(node->next != NULL)
+		{
+			if (node->key != key)
+				node = node->next;
+			else
+			{
+				node->value = strdup(value);
+				return (1);
+			}
+		}
+		new_node = create_node(key, value);
+		new_node->next = ht->array[index];	
+		ht->array[index] = new_node;
 	}
-
 	return (1);
 }
